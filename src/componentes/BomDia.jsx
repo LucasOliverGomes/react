afc28086ee05efe400d122c19b1ffff37666cdad
LucasOliverGomes <lucasudioliveira2020@gmail.com>
2019-08-02 14:19:34 -0300

    test

---
+++ src/componentes/BomDia.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default () => <h1>Bom Dia gente boa</h1>;
---
+++ src/componentes/Primeiro.jsx
@@ -0,0 +1,3 @@
+import React from 'reacr';
+//Para usar uma tag deve importar o React
+export default () => <h1> "A vida e bela"</h1>;
