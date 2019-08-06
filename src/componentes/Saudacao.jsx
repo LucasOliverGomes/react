0b1ddc6270e10aaba508a38ff2e3d492527b7d4c
KhavVicttor <khav.prfs@gmail.com>
2019-08-06 14:09:58 -0300

    teste

--- src/componentes/Saudacao.jsx
+++ src/componentes/Saudacao.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import { runInThisContext } from 'vm';
 
 export default class saudacao extends Component {
+  state = {
+    tipo: this.props.tipo,
+    nome: this.props.nome
+  };
+
+  setTipo(e) {
+    this.setState({ tipo: e.target.value });
+  }
+  setNome(e) {
+    this.serState({ tipo: e.target.value });
+  }
   render() {
-    const { tipo, nome } = this.props;
+    const { tipo, nome } = this.state;
     return (
       <div>
         <h1>
@@ -10,8 +22,8 @@ export default class saudacao extends Component {
           {nome}
         </h1>
         <hr />
-        <input type="text" placeholder="Tipo..." value={tipo} />
-        <input type="text" placeholder="Tipo..." value={nome} />
+        <input type="text" placeholder="Tipo..." value={tipo} onCharge={e => this.setTipo(e)} />
+        <input type="text" placeholder="Nome..." value={nome} onCharge={e => this.setNome(e)} />
       </div>
     );
   }
